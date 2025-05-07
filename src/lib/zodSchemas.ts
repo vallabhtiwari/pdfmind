ea7df887927bc1aceeb74a0e36e2a1f765f25f93
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-05-07 10:09:11 +0530

    type and zod schema for pdf file

---
+++ src/lib/zodSchemas.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const pdfFileSchema = z.object({
+  file: z
+    .instanceof(File)
+    .refine((file) => file.type === "application/pdf", {
+      message: "Invalid PDF",
+    })
+    .refine((file) => file.name.endsWith(".pdf"), { message: "Invalid PDF" }),
+});
