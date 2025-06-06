4ba8ca37b15b7519609842376e3fcc4f56d465ec
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-06-06 20:41:49 +0530

    gemini for generating user friendly response

--- src/store/models.ts
+++ src/store/models.ts
@@ -1,6 +1,7 @@
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { Chroma } from "@langchain/community/vectorstores/chroma";
+import { GoogleGenAI } from "@google/genai";
 
 export const splitter = new RecursiveCharacterTextSplitter({
   chunkSize: 500,
@@ -14,3 +15,5 @@ export const vectorStore = new Chroma(embedder, {
   collectionName: "pdfCollection",
   url: process.env.CHROMADB_URL,
 });
+
+export const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
