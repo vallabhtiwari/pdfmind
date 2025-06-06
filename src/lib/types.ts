677fdf366f0b036e9929a46d55ed5c3dd289c0d0
Vallabh Tiwari <tiwarivallabh27@gmail.com>
2025-06-06 22:32:07 +0530

    types for chat store

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,11 @@
 export type PDFFile = string | File | null;
+
+export type MessageFrom = "bot" | "user";
+
+export type Message = {
+  id: string;
+  message: string;
+  from: MessageFrom;
+};
+
+export type ChatsType = Message[];
